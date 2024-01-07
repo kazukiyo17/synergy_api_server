baa9f76549449dd59650a7ec503570e2989e60cc
p_xyunli <p_xyunli@tencent.com>
2024-01-08 00:52:45 +0800

    加Domain

--- start.sh
+++ start.sh
@@ -3,4 +3,4 @@ docker rm synergy_api_server
 docker rmi synergy_api_server
 docker build -t synergy_api_server .
 #docker pull registry.cn-shanghai.aliyuncs.com/game_xyunli/synergy_api_server:latest
-docker run -d --name synergy_api_server -p 8081:8081 --network game-network synergy_api_server
+docker run -d --name synergy_api_server -p 443:443 --network game-network synergy_api_server
